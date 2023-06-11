723b1b82517c6dcda7a0c6b4b880f8fc925aff87
ping-yee <611077101@mail.nknu.edu.tw>
2023-06-11 20:38:16 +0800

    tests: All todoList testcases have been implemented.

--- app/tests/Api/V1/TodoListsTest.php
+++ app/tests/Api/V1/TodoListsTest.php
@@ -80,14 +80,125 @@ class TodoListsTest extends DatabaseTestCase
 
     public function testShowAllDataSuccessfully()
     {
-        # code...
+        $createData = [
+            "title"     => "Example Title",
+            "content"   => "Example Content",
+        ];
+
+        $todoListsModel = new TodoListsModel();
+
+        $now = date("Y-m-d H:i:s");
+
+        $createdKey = $todoListsModel->insert([
+            "t_title"    => $createData["title"],
+            "t_content"  => $createData["content"],
+            "m_key"      => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
+
+        $this->assertEquals(1, $createdKey);
+
+        $createSecondData = [
+            "title"     => "Example Title 2",
+            "content"   => "Example Content 2",
+        ];
+
+        $createdSecondKey = $todoListsModel->insert([
+            "t_title"   => $createSecondData["title"],
+            "t_content" => $createSecondData["content"],
+            "m_key"     => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
+
+        $this->assertEquals(2, $createdSecondKey);
+
+        $results = $this->withSession($this->sessionData)
+                        ->get("api/v1/todo");
+
+        $returnData = json_decode($results->getJSON());
+
+        $excepted = [
+            [
+                "t_key"      => "1",
+                "t_title"    => "Example Title",
+                "t_content"  => "Example Content",
+                "m_key"      => "1",
+                "created_at" => $now,
+                "updated_at" => $now,
+                "deleted_at" => null
+            ],
+            [
+                "t_key"      => "2",
+                "t_title"    => "Example Title 2",
+                "t_content"  => "Example Content 2",
+                "m_key"      => "1",
+                "created_at" => $now,
+                "updated_at" => $now,
+                "deleted_at" => null
+            ]
+        ];
+
+        $this->assertEquals($excepted[0], (array)$returnData->data[0]);
+        $this->assertEquals($excepted[1], (array)$returnData->data[1]);
+    }
+
+    public function testShowSingleDataSuccessfully()
+    {
+        $createData = [
+            "title"     => "Example Title",
+            "content"   => "Example Content",
+        ];
+
+        $todoListsModel = new TodoListsModel();
+
+        $now = date("Y-m-d H:i:s");
+
+        $createdKey = $todoListsModel->insert([
+            "t_title"    => $createData["title"],
+            "t_content"  => $createData["content"],
+            "m_key"      => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
+
+        $this->assertEquals(1, $createdKey);
+
+        $createSecondData = [
+            "title"     => "Example Title 2",
+            "content"   => "Example Content 2",
+        ];
+
+        $createdSecondKey = $todoListsModel->insert([
+            "t_title"   => $createSecondData["title"],
+            "t_content" => $createSecondData["content"],
+            "m_key"     => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
+
+        $this->assertEquals(2, $createdSecondKey);
+
+        $results = $this->withSession($this->sessionData)
+                        ->get("api/v1/todo/2");
+
+        $returnData = json_decode($results->getJSON());
+
+        $excepted = [
+            "key"      => "2",
+            "title"    => "Example Title 2",
+            "content"  => "Example Content 2"
+        ];
+
+        $this->assertEquals($excepted, (array)$returnData->data);
     }
 
     public function testUpdateTodoSuccessfully()
     {
         $createData = [
-            "title"     => "Example Title 2",
-            "content"   => "Example Content 2",
+            "title"     => "Example Title",
+            "content"   => "Example Content",
         ];
 
         $todoListsModel = new TodoListsModel();
@@ -129,5 +240,61 @@ class TodoListsTest extends DatabaseTestCase
         ];
 
         $this->assertEquals($excepted, (array)$returnData);
+    }
+
+    public function FunctionName()
+    {
+        $createData = [
+            "title"     => "Example Title",
+            "content"   => "Example Content",
+        ];
+
+        $todoListsModel = new TodoListsModel();
+
+        $now = date("Y-m-d H:i:s");
+
+        $createdKey = $todoListsModel->insert([
+            "t_title"    => $createData["title"],
+            "t_content"  => $createData["content"],
+            "m_key"      => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
+
+        $this->assertEquals(1, $createdKey);
+
+        $createSecondData = [
+            "title"     => "Example Title 2",
+            "content"   => "Example Content 2",
+        ];
+
+        $createdSecondKey = $todoListsModel->insert([
+            "t_title"   => $createSecondData["title"],
+            "t_content" => $createSecondData["content"],
+            "m_key"     => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
+
+        $this->assertEquals(2, $createdSecondKey);
+
+        $results = $this->withSession($this->sessionData)
+                        ->delete("api/v1/todo/2");
+
+        $returnData = json_decode($results->getJSON());
+
+        $excepted = [
+            "msg" => "Delete successfully"
+        ];
+
+        $this->assertEquals($excepted, (array)$returnData->msg);
+
+        $this->dontSeeInDatabase('TodoLists', [
+            "t_title"   => $createSecondData["title"],
+            "t_content" => $createSecondData["content"],
+            "m_key"     => 1,
+            "created_at" => $now,
+            "updated_at" => $now,
+        ]);
     }
 }
